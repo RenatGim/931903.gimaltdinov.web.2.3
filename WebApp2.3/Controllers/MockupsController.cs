f3a4b303701a3f31dc57b1f69f1e4c7929f5b97e
Gimaltdinov <96269132+RenatGim@users.noreply.github.com>
2022-07-04 22:29:56 +0700

    Add files via upload

---
+++ WebApp2.3/Controllers/MockupsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp2._3.Models;
+
+namespace WebApp2._3.Controllers
+{
+    public class MockupsController : Controller
+    {
+        private readonly ILogger<MockupsController> _logger;
+
+        public MockupsController(ILogger<MockupsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Mockups()
+        {
+            return View();
+        }
+
+        static readonly QuizViewModel quiz = new QuizViewModel();
+
+        public IActionResult Quiz()
+        {
+            quiz.SetRandomValues();
+            ViewBag.Question = quiz.question;
+            return View();
+        }
+
+        [ActionName("Result")]
+        public IActionResult QuizResult()
+        {
+            ViewBag.RightAnswersCount = quiz.rightAnswersCount;
+            ViewBag.AnswersCount = quiz.answersCount;
+            ViewBag.Results = quiz.Results;
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Quiz")]
+        public IActionResult QuizNext(int answer)
+        {
+            quiz.UppdateResults(answer);
+            quiz.SetRandomValues();
+            ViewBag.Question = quiz.question;
+            return View("Quiz");
+        }
+
+        public IActionResult QuizFinish(int answer)
+        {
+            quiz.UppdateResults(answer);
+            ViewBag.RightAnswersCount = quiz.rightAnswersCount;
+            ViewBag.AnswersCount = quiz.answersCount;
+            ViewBag.Results = quiz.Results;
+            return View("Result");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp2.3/Models/QuizViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace WebApp2._3.Models
+{
+    public class QuizViewModel
+    {
+        string[] operations = { "+", "-", "*" };
+
+        private int X;
+        private int Y;
+        private string operation;
+        private int rightAnswer { get; set; }
+        public string question;
+        public List<string> Results { get; set; } = new List<string>();
+
+        public int answersCount = 0;
+        public int rightAnswersCount = 0;
+
+
+        public void SetRandomValues()
+        {
+            Random rand = new Random();
+            X = rand.Next(0, 15);
+            Y = rand.Next(0, 15);
+            operation = operations[rand.Next(0, 3)];
+            switch (operation)
+            {
+                case "+":
+                    rightAnswer = X + Y; break;
+                case "-":
+                    rightAnswer = X - Y; break;
+                case "*":
+                    rightAnswer = X * Y; break;
+            };
+            question = $"{X} {operation} {Y} = ";
+        }
+
+        public void UppdateResults(int answer)
+        {
+            answersCount++;
+            if (answer == rightAnswer)
+            {
+                rightAnswersCount++;
+            }
+            Results.Add($"{question} {answer}");
+        }
+    }
+}
